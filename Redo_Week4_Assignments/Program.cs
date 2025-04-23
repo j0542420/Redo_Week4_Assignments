d9d88fc0a45f163d0d9935b8fcbd9e7fdbdacd1e
Jose Villa <j0542420@amarillocollege.com>
2025-04-23 12:58:02 -0500

    Add project files.

---
+++ Redo_Week4_Assignments/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redo_Week4_Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // while loop to return to program menu
+            while (true)
+            {
+                // display menu
+                Console.WriteLine();
+            }
+        }
+    }
+}
